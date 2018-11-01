803fdf7bd5d5bd47474859868092815817579e60
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-01 20:24:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return programmer_hash[:dennis_ritchie][:languages].values[0]
 end
 
 def adding_matz
@@ -82,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:yukihiro_matsumoto]={known_for: "Ruby", languages: ["LISP", "C"]}
 
 end
 
